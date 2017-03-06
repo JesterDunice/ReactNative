4e86c95cfed361bba73608fa9deb4e0ebfcd93e2
JesterDunice <nikita.dudin@gmail.com>
2017-03-06 16:45:07 +0300

    3rd iter

--- TeamList.js
+++ TeamList.js
@@ -4,32 +4,40 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, TextInput, View} from 'react-native';
 
-class GamesDisplay extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    return (
-      <View>
-        <Text style={styles.game_anounce}>
-          {Shedule.map((row) => row.host.name + '-' + row.guest.name + '\n' + row.host.city + '-' + row.guest.city + '\n'
-          )}
-        </Text>
-      </View>)
-  }
-}
-
-const styles = {
-  game_anounce: {padding: 10, fontSize: 20, color: 'red'},
-  game_date: {padding: 10, fontSize: 20, color: 'blue'},
-}
 
 const Shedule = [
   {host: {name: 'h1', city: 'hc1'}, guest: {name: 'g1', city: 'gc1'}, date: "11.11.1111"},
   {host: {name: 'h2', city: 'hc2'}, guest: {name: 'g2', city: 'gc2'}, date: "22.22.2222"},
   {host: {name: 'h3', city: 'hc3'}, guest: {name: 'g3', city: 'gc3'}, date: "33.33.3333"},
 ];
+
+const styles = {
+  game_anounce: {padding: 10, fontSize: 20, color: 'red'},
+  game_date: {padding: 10, fontSize: 20, color: 'blue'},
+  game_city: {padding: 10, fontSize: 20, color: 'green'}
+}
+
+export default class TeamList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      element: this.props.line
+    };
+  }
+
+  render() {
+    return (
+          <View>
+            <Text style={styles.game_anounce}>{this.state.element.host.name + '-' + this.state.element.guest.name}</Text>
+            <Text style={styles.game_city}>{this.state.element.host.city + '-' + this.state.element.guest.city}</Text>
+            <Text style={styles.game_date}>{this.state.element.date}</Text>
+          </View>
+    );
+  }
+}
+
+
+
 
 
--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,47 @@
 //AwesomeProject
 import React, {Component} from 'react';
+import TeamList from './TeamList.js';
 import {AppRegistry, Text, TextInput, View} from 'react-native';
 
-import TeamList from 'TeamList.js';
 
 class GamesDisplay extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listofteams: []
+    };
+
   }
 
+  componentDidMount() {
+    const listofteams = ListOfPlayingTeams;
+    this.setState({listofteams});
+  }
 
   render() {
     return (
       <View>
-      <Text style={styles.game_anounce}>
-        {Shedule.map((row) => row.host.name + '-' + row.guest.name + '\n' + row.host.city + '-' + row.guest.city + '\n'
-        )}
-      </Text>
-     </View>)
+        <Text>test</Text>
+        <View style={{alignItems: 'center'}}>
+          {this.state.listofteams.map(game =>
+            <TeamList line={game} />
+          )}
+        </View>
+      </View>
+    )
   }
 }
 
-const styles = {
-  game_anounce: {padding: 10, fontSize: 20, color: 'red'},
-  game_date: {padding: 10, fontSize: 20, color: 'blue'},
-}
-
-const Shedule = [
+const ListOfPlayingTeams = [
   {host: {name: 'h1', city: 'hc1'}, guest: {name: 'g1', city: 'gc1'}, date: "11.11.1111"},
   {host: {name: 'h2', city: 'hc2'}, guest: {name: 'g2', city: 'gc2'}, date: "22.22.2222"},
   {host: {name: 'h3', city: 'hc3'}, guest: {name: 'g3', city: 'gc3'}, date: "33.33.3333"},
 ];
 
+
+
 AppRegistry.registerComponent('AwesomeProject', () => GamesDisplay);
+
 
 
 
